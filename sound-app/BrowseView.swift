74e292c1c6ed321ebc16209fe6e255fee954fa02
SaakethC <saakethrchenna@gmail.com>
2021-03-13 16:18:31 -0600

    tried adding animations to onboarding

---
+++ sound-app/BrowseView.swift
@@ -0,0 +1,26 @@
+//
+//  BrowseView.swift
+//  sound-app
+//
+//  Created by Arpan Dhatt on 3/13/21.
+//
+
+import SwiftUI
+
+struct BrowseView: View {
+    var body: some View {
+        VStack {
+            Image("cart")
+                .resizable()
+                .scaledToFit()
+            TitleViewBold(input: "Coming Soon")
+            ParagraphView(input: "We didn't have the time :(")
+        }
+    }
+}
+
+struct BrowseView_Previews: PreviewProvider {
+    static var previews: some View {
+        BrowseView()
+    }
+}
