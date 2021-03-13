1a89a7f35f9f4541ce7d7bf22307ed6b77b84295
arpan-dhatt <ary.dhatt@gmail.com>
2021-03-13 15:18:43 -0600

    Initial Commit

---
+++ sound-app/sound_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  sound_appApp.swift
+//  sound-app
+//
+//  Created by Arpan Dhatt on 3/13/21.
+//
+
+import SwiftUI
+
+@main
+struct sound_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
