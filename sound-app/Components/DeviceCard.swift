b744331f1a4a8b951bcbcc82f2a204dbb351bd40
arpan-dhatt <ary.dhatt@gmail.com>
2021-03-14 13:45:57 -0500

    final changes to ui

--- sound-app/Components/DeviceCard.swift
+++ sound-app/Components/DeviceCard.swift
@@ -27,28 +27,7 @@ struct DeviceCard: View {
                     Spacer()
                 }.frame(width: 150).padding(.trailing)
                 
-                VStack(alignment:.leading){
-                    if device.activeSounds.count == 0{
-                        Text("This Device Has No Active Sounds").multilineTextAlignment(.leading)
-                    }
-                    else if device.activeSounds.count < 4{
-                        ParagraphViewBold(input: "Active Sounds:")
-                        ForEach(device.activeSounds, id: \.self){
-                            Text("- \($0)")
-                        }.multilineTextAlignment(.leading)
-                        
-                    }
-                    else{
-                        ParagraphViewBold(input: "Active Sounds:")
-                        ForEach(0..<3, id: \.self){
-                            Text("- \(device.activeSounds[$0])")
-                        }.multilineTextAlignment(.leading)
-                        HStack{
-                            Text("More").bold()
-                            Image(systemName: "arrow.right")
-                        }
-                    }
-                }
+                
             }
             
         }.padding().frame(height: 175).background(PurposeColors.getColor[device.purpose]).cornerRadius(10.0).padding(.horizontal).padding(.vertical, 10).shadow(radius: 10).foregroundColor(.white)
